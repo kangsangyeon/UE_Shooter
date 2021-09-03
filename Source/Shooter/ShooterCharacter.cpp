b8bff0445b02daf7d31ea9baf0936a066cb2665c
sanyoni <ksyline1997@gmail.com>
2021-09-04 02:10:21 +0900

    comment: 더 이상 필요하지 않은 LOG 코드를 삭제하였습니다.

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -80,33 +80,6 @@ void AShooterCharacter::BeginPlay()
 		CameraDefaultFOV = GetFollowCamera()->FieldOfView;
 		CameraCurrentFOV = CameraDefaultFOV;
 	}
-
-
-	UE_LOG(LogTemp, Warning, TEXT("BeginPlay() called!"));
-	int myInt{42};
-	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
-
-	float myFloat{3.14159f};
-	UE_LOG(LogTemp, Warning, TEXT("float myFloat: %f"), myFloat);
-
-	double myDouble{0.000756};
-	UE_LOG(LogTemp, Warning, TEXT("double myDouble: %lf"), myDouble);
-
-	char myChar{'3'};
-	UE_LOG(LogTemp, Warning, TEXT("char myChar: %c"), myChar);
-
-	wchar_t myWideChar{L'4'};
-	UE_LOG(LogTemp, Warning, TEXT("w_char myWideChar: %lc"), myWideChar);
-
-	bool myBool{true};
-	UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), myBool);
-
-	UE_LOG(LogTemp, Warning, TEXT("int %d, float: %f, bool: %d"), myInt, myFloat, myBool);
-
-	FString myString{TEXT("My String!!!!")};
-	UE_LOG(LogTemp, Warning, TEXT("FString myString: %s"), *myString);
-
-	UE_LOG(LogTemp, Warning, TEXT("Name of instance: %s"), *GetName());
 }
 
 void AShooterCharacter::MoveForward(float Value)
@@ -159,8 +132,6 @@ void AShooterCharacter::LookUp(float Value)
 
 void AShooterCharacter::FireWeapon()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Fire Weapon"));
-
 	if (FireSound)
 	{
 		UGameplayStatics::PlaySound2D(this, FireSound);
@@ -306,9 +277,6 @@ void AShooterCharacter::CalculateCrosshairSpread(float DeltaTime)
 		+ CrosshairInAirFactor
 		+ CrosshairAimFactor,
 		0.f, BIG_NUMBER);
-
-	if (GEngine)
-		GEngine->AddOnScreenDebugMessage(10, 0, FColor::White, FString::Printf(TEXT("Crosshair Spread Multiplier: %f"), CrosshairSpreadMultiplier));
 }
 
 // Called every frame
